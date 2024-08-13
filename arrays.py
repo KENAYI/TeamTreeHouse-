421801d194f505c790f434dea074e65c34b02d5a
KENAYI <chikenayiweli@gmail.com>
2024-08-13 16:33:32 +0100

    arrays and various operations

---
+++ arrays.py
@@ -0,0 +1,15 @@
+# Search through array
+
+new_list = [1,2,3]
+result = new_list[0]
+
+# Check that first index in array contains "1"
+for n in new_list:
+    if n == 1:
+        print (True)
+        break
+    
+
+numbers = []
+numbers.extend([4,5,6])
+print(numbers)
