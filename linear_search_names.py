183b2726ebf1d762e6540d3b6ee00d267654daf9
KENAYI <chikenayiweli@gmail.com>
2024-08-15 16:29:28 +0100

    updating name search proof

--- linear_search_names.py
+++ linear_search_names.py
@@ -12,7 +12,7 @@ def index_of_item(collection, target):
     for i in range(0, len(collection)):
         if target == collection[i]:
             return i
-        return None
+    return None
 
 for n in search_names:
     index = index_of_item(names, n)
